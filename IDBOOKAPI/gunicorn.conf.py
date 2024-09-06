58006a7351983a7f2a3c21ec3252768a82ee9856
vignesh-cloud-prog <vigneshun80@gmail.com>
2024-09-06 17:36:33 +0530

    dockerise django app

--- IDBOOKAPI/gunicorn.conf.py
+++ IDBOOKAPI/gunicorn.conf.py
@@ -1,13 +1,4 @@
-# gunicorn.conf.py
-# Non logging stuff
-# bind = "68.178.161.66:8080"
-bind = "0.0.0.0:8080"
+bind = '0.0.0.0:8000'
 workers = 3
-# Access log - records incoming HTTP requests
-accesslog = "/var/log/gunicorn.access.log"
-# Error log - records Gunicorn server goings-on
-errorlog = "/var/log/gunicorn.error.log"
-# Whether to send Django output to the error log
-capture_output = True
-# How verbose the Gunicorn error logs should be
-loglevel = "info"
+loglevel = 'info'
+
