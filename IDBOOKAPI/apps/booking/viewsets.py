a29832b370957acf292ce1aba0607f696c007c9b
Sonu George <george.sonu1@gmail.com>
2024-10-17 14:45:55 +0530

    company search and pagination, booking status filter and pagination

--- IDBOOKAPI/apps/booking/viewsets.py
+++ IDBOOKAPI/apps/booking/viewsets.py
@@ -16,18 +16,28 @@ from .models import (Booking, AppliedCoupon)
 from apps.booking.tasks import send_booking_email_task
 
 from rest_framework.decorators import action
+from django.db.models import Q
 
 
 class BookingViewSet(viewsets.ModelViewSet, StandardResponseMixin, LoggingMixin):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    # permission_classes = [AnonymousCanViewOnlyPermission,]
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['property', 'room', 'user', 'coupon', 'booking_type', 'room_type', 'checkin_time',
-                        'checkout_time', 'bed_count', 'person_capacity', 'child_capacity', 'deal_price', 'discount',
-                        'final_amount', 'status', 'active', 'created', 'updated']
-    http_method_names = ['get', 'post', 'put', 'patch']
+    permission_classes = [IsAuthenticated,]
+##    filter_backends = [DjangoFilterBackend]
+##    filterset_fields = ['property', 'room', 'user', 'coupon', 'booking_type', 'room_type', 'checkin_time',
+##                        'checkout_time', 'bed_count', 'person_capacity', 'child_capacity', 'deal_price', 'discount',
+##                        'final_amount', 'status', 'active', 'created', 'updated']
+##    http_method_names = ['get', 'post', 'put', 'patch']
     # lookup_field = 'custom_id'
+
+##    permission_classes_by_action = {'create': [IsAuthenticated], 'update': [IsAuthenticated]}
+##
+##    def get_permissions(self):
+##        try: 
+##            return [permission() for permission in self.permission_classes_by_action[self.action]]
+##        except KeyError: 
+##            # action is not set return default permission_classes
+##            return [permission() for permission in self.permission_classes]
 
     def create(self, request, *args, **kwargs):
         self.log_request(request)  # Log the incoming request
@@ -96,6 +106,23 @@ class BookingViewSet(viewsets.ModelViewSet, StandardResponseMixin, LoggingMixin)
 
     def list(self, request, *args, **kwargs):
         self.log_request(request)  # Log the incoming request
+        print("Inside Booking")
+
+        offset = int(self.request.query_params.get('offset', 0))
+        limit = int(self.request.query_params.get('limit', 10))
+        booking_status = self.request.query_params.get('status', '')
+##        search = request.query_params.get('search', '')
+##
+##        if search:
+##            search_q_filter = Q(company_name__icontains=search) | Q(brand_name__icontains=search)
+##            self.queryset = self.queryset.filter(search_q_filter)
+
+        if booking_status:
+            self.queryset = self.queryset.filter(status=booking_status)
+        
+
+        count = self.queryset.count()
+        self.queryset = self.queryset[offset:offset+limit]
 
         # Perform the default listing logic
         response = super().list(request, *args, **kwargs)
@@ -105,6 +132,7 @@ class BookingViewSet(viewsets.ModelViewSet, StandardResponseMixin, LoggingMixin)
             custom_response = self.get_response(
                 data=response.data,  # Use the data from the default response
                 message="List Retrieved",
+                count=count,
                 status_code=status.HTTP_200_OK,  # 200 for successful listing
 
             )
@@ -150,11 +178,24 @@ class BookingViewSet(viewsets.ModelViewSet, StandardResponseMixin, LoggingMixin)
             url_path='user/retrieve', url_name='user-retrieve')
     def user_based_retrieve(self, request):
   
-        booking = self.queryset.filter(user=request.user)
-        booking_serializer = BookingSerializer(booking, many=True)
+        self.queryset = self.queryset.filter(user=request.user)
+        
+        offset = int(self.request.query_params.get('offset', 0))
+        limit = int(self.request.query_params.get('limit', 10))
+        booking_status = self.request.query_params.get('status', '')
+
+        if booking_status:
+            self.queryset = self.queryset.filter(status=booking_status)
+        
+
+        count = self.queryset.count()
+        self.queryset = self.queryset[offset:offset+limit]
+        
+        booking_serializer = BookingSerializer(self.queryset, many=True)
         
         response = self.get_response(
             data=booking_serializer.data, status="success", message="Retrieve Booking Success",
+            count=count,
             status_code=status.HTTP_200_OK,
             )
         return response
