56d63dbce8a5adbc33588d3ef7b55b61652f35c2
Sonu George <george.sonu1@gmail.com>
2024-11-02 15:57:54 +0530

    total amount calculation and apply copoun, email and notification for cancel booking(hotel, hotelpackage, vehicle,flight

--- IDBOOKAPI/apps/booking/signals.py
+++ IDBOOKAPI/apps/booking/signals.py
@@ -1,10 +1,18 @@
 # code
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Booking
-from apps.booking.tasks import send_booking_email_task, create_invoice_task
-from apps.booking.utils.booking_utils import generate_booking_confirmation_code
+from apps.booking.tasks import (
+        send_booking_email_task, create_invoice_task,
+        send_cancelled_booking_task)
+from apps.booking.utils.booking_utils import (
+        generate_booking_confirmation_code, calculate_total_amount)
 import time
+
+@receiver(pre_save, sender=Booking)
+def update_total_amount(sender, instance:Booking, **kwargs):
+        total_booking_amount = calculate_total_amount(instance)
+        instance.final_amount = total_booking_amount
 
 @receiver(post_save, sender=Booking) 
 def check_booking_status(sender, instance:Booking, **kwargs):
@@ -25,6 +33,8 @@ def check_booking_status(sender, instance:Booking, **kwargs):
                         send_booking_email_task.apply_async(args=[booking_id, booking_type])
                         # time.sleep(3)
                         create_invoice_task.apply_async(args=[booking_id])
+                if booking_status == 'canceled':
+                        send_cancelled_booking_task.apply_async(args=[booking_id])
 	
 
 
--- IDBOOKAPI/apps/org_resources/utils/notification_utils.py
+++ IDBOOKAPI/apps/org_resources/utils/notification_utils.py
@@ -19,6 +19,25 @@ The confirmation code is: {confirmation_code}".format(booking_type=booking_type,
 
     return notification_dict
 
+def booking_cancelled_notification_template(booking_id, booking_type, cancellation_code, notification_dict):
+    try:
+        # Notification
+        title = "{booking_type} Booking Cancelled".format(booking_type=booking_type)
+        description = " We regret to inform you that your {booking_type} booking has been successfully cancelled.\
+The cancellation code is: {cancellation_code}".format(booking_type=booking_type,
+                                                      cancellation_code=cancellation_code)
+        redirect_url = "/booking/bookings/{booking_id}/".format(booking_id=booking_id)
+     
+        
+        notification_dict['title'] = title
+        notification_dict['description'] = description
+        notification_dict['redirect_url'] = redirect_url
+        
+    except Exception as e:
+        print('Notification Error', e)
+
+    return notification_dict
+
 def wallet_minbalance_notification_template(wallet_balance, notification_dict):
     try:
         title = "Low Wallet Balance Notification"
