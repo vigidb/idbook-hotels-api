440a1fbc0ed66760d8ee0ad07ff82924e9f6f098
Sonu George <george.sonu1@gmail.com>
2025-01-09 10:24:22 +0530

    property analytics for checkin and revenue

--- IDBOOKAPI/apps/analytics/utils/db_utils.py
+++ IDBOOKAPI/apps/analytics/utils/db_utils.py
@@ -22,11 +22,10 @@ def create_or_update_property_count(
     except Exception as e:
         print(e)
 
-def get_property_visit(property_id, date=None, no_of_days=None):
+def get_property_visit(property_id, date=None, start_date=None, end_date=None):
     prop_analytics_obj = PropertyAnalytics.objects.filter(
         property_info=property_id)
 
-    print(prop_analytics_obj)
 
     date_visit_count = 0
     drange_visit_count = 0
@@ -35,12 +34,7 @@ def get_property_visit(property_id, date=None, no_of_days=None):
         prop_analytics_date_obj = prop_analytics_obj.filter(created__date=date)
         date_visit_count = prop_analytics_date_obj.count()
 
-    if no_of_days:
-        end_date = datetime.now(timezone('UTC'))
-        start_date = datetime.now(timezone('UTC')) - timedelta(days=7)
-        end_date = end_date.date()
-        start_date = start_date.date()
-        
+    if start_date and end_date:
         prop_analytics_drange_obj = prop_analytics_obj.filter(
             created__date__lte=end_date, created__date__gte=start_date)
 
