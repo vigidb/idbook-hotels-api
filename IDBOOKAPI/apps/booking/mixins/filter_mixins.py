b842455aeded4068866b9386bad32d2fbfd44b45
Sonu George <george.sonu1@gmail.com>
2025-01-25 15:27:37 +0530

    booking payment api modification

--- IDBOOKAPI/apps/booking/mixins/filter_mixins.py
+++ IDBOOKAPI/apps/booking/mixins/filter_mixins.py
@@ -5,11 +5,8 @@ class PaymentPropertyFilterMixins:
 
     def payment_property_filter_ops(self):
         filter_dict = {}
+        booking_payment_filter_enabled = False
 
-        self.queryset = self.queryset.filter(
-            Q(booking_payment__booking__isnull=False)
-            |Q(booking_payment__booking__isnull=True))
-        
         param_dict = self.request.query_params
         for key in param_dict:
             param_value = param_dict[key]
@@ -20,9 +17,33 @@ class PaymentPropertyFilterMixins:
             elif key == 'booking_status':
                 status_list = param_value.split(',')
                 filter_dict['status__in'] = status_list
+            elif key == 'start_checkin_date':
+                start_checking_date = param_value.replace(' ', '+')
+                filter_dict['hotel_booking__confirmed_checkin_time__gte'] = start_checking_date
+            elif key == 'end_checkin_date':
+                end_checkin_date = param_value.replace(' ', '+')
+                filter_dict['hotel_booking__confirmed_checkin_time__lte'] = end_checkin_date
+            elif key == 'is_transaction_success':
+                is_transaction_success = True if param_value == "true" else False
+                filter_dict['booking_payment__is_transaction_success'] = is_transaction_success
+                booking_payment_filter_enabled = True
 
-        
                 
         if filter_dict:
             self.queryset = self.queryset.filter(**filter_dict)
+
+        return booking_payment_filter_enabled
+
+    def payment_property_search_ops(self):
+        search_value = self.request.query_params.get('search', '')
+        if search_value:
+            self.queryset = self.queryset.filter(Q(reference_code__icontains=search_value))
+            
+
+    def left_join_booking_payment_ops(self):
+
+        self.queryset = self.queryset.filter(
+            Q(booking_payment__booking__isnull=False)
+            |Q(booking_payment__booking__isnull=True))
+        
 
--- IDBOOKAPI/apps/booking/subviews/payment_viewset.py
+++ IDBOOKAPI/apps/booking/subviews/payment_viewset.py
@@ -1,7 +1,8 @@
 from .__init__ import *
 
 from apps.booking.models import Booking
-from apps.booking.serializers import PropertyPaymentBookingSerializer
+from apps.booking.serializers import (
+    PropertyPaymentBookingSerializer, PaymentMediumSerializer)
 
 from apps.booking.mixins.filter_mixins import PaymentPropertyFilterMixins
 from apps.booking.mixins.booking_db_mixins import CommonDbMixins
@@ -16,28 +17,45 @@ class PaymentPropertyViewSet(viewsets.ModelViewSet, PaymentPropertyFilterMixins,
     def list(self, request, *args, **kwargs):
 
         # filter
-        self.payment_property_filter_ops()
+        booking_payment_filter_enabled = self.payment_property_filter_ops()
+        # search
+        self.payment_property_search_ops()
         
         # booking payment aggregate
         self.booking_payment_aggregate()
-        booking_payment_agg = {"total_booking_amount": self.total_booking_amount,
-                               "total_payment_made": self.total_payment_made}
+        # payment medium split
+        booking_payment_queryset = self.total_payment_mode()
+        pmedium_serializer = PaymentMediumSerializer(booking_payment_queryset, many=True)
 
+        booking_payment_agg = {"total_booking_amount": self.total_booking_amount,
+                               "total_payment_made": self.total_payment_made,
+                               'payment_medium':pmedium_serializer.data}
+
+        
+        # combination of booking and booking payment
+        if not booking_payment_filter_enabled:
+            self.left_join_booking_payment_ops()
+    
         self.queryset = self.queryset.order_by('id')
-        count = self.queryset.count()
+        count, self.queryset = paginate_queryset(self.request,  self.queryset)
+        # print("query", self.queryset.query)
+        
+        # count = self.queryset.count()
 
         booking_payment_dict = self.queryset.values(
-            'id', 'reference_code', 'confirmation_code', 'invoice_id',
+            'id', 'reference_code', 'confirmation_code', 'invoice_id', 'final_amount', 'total_payment_made',
             'hotel_booking__confirmed_checkin_time', 'hotel_booking__confirmed_checkout_time',
             'booking_payment__merchant_transaction_id', 'booking_payment__payment_type',
             'booking_payment__payment_medium', 'booking_payment__amount',
             'booking_payment__is_transaction_success')
         serializer = PropertyPaymentBookingSerializer(booking_payment_dict, many=True)
         
+        data = {"payment_aggregate": booking_payment_agg, "booking_payment_details": serializer.data}
+        
         
         custom_response = self.get_response(
             status="success",
-            data=serializer.data,  # Use the data from the default response
+            data=data,  # Use the data from the default response
             message="List Retrieved",
             count=count,
             status_code=status.HTTP_200_OK,  # 200 for successful listing
