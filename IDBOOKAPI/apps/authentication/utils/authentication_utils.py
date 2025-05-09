554cb3ab98714ddda8b6b39217a0995042fdb23e
Sonu George <george.sonu1@gmail.com>
2025-05-09 15:22:31 +0530

    more subscription details while login

--- IDBOOKAPI/apps/authentication/utils/authentication_utils.py
+++ IDBOOKAPI/apps/authentication/utils/authentication_utils.py
@@ -27,11 +27,22 @@ def user_representation(user, refresh_token=None):
 
     subscription_name = ""
     user_subscription_id = None
+    sub_start_date = None
+    sub_end_date = None
+    subscription_amount = None
+    last_paid_date = None
+    next_payment_date = None
     user_subscription = user.user_subscription.filter(active=True).last()
     user_subscription_details = {}
     if user_subscription:
         subscription_name = user_subscription.idb_sub.name
         user_subscription_id = user_subscription.id
+        sub_start_date = user_subscription.sub_start_date
+        sub_end_date = user_subscription.sub_end_date
+        subscription_amount = user_subscription.subscription_amount
+        last_paid_date = user_subscription.last_paid_date
+        next_payment_date = user_subscription.next_payment_date
+        
 
     user_data = {'id': user.id, 'mobile_number': user.mobile_number if user.mobile_number else '',
                  'email': user.email if user.email else '', 'name': user.get_full_name(),
@@ -40,7 +51,9 @@ def user_representation(user, refresh_token=None):
                  'business_id': user.business_id if user.business_id else '',
                  'company_id' : user.company_id if user.company_id else '',
                  'default_group': user.default_group, 'subscription_name':subscription_name,
-                 'user_subscription_id':user_subscription_id,
+                 'user_subscription_id':user_subscription_id, "sub_start_date":sub_start_date,
+                 "sub_end_date":sub_end_date, "subscription_amount":subscription_amount,
+                 "last_paid_date":last_paid_date, "next_payment_date":next_payment_date,
                  'is_active': user.is_active}
 
     if refresh_token:
